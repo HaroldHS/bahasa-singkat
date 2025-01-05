c8a4dbbd6277d16b9d459cc3e67af5b4c553417c
HaroldHS <haroldhs.hh@gmail.com>
2025-01-05 14:14:50 +0700

    Resolve mmap errors in virtual machine

--- compiler/VirtualMachine.go
+++ compiler/VirtualMachine.go
@@ -53,19 +53,24 @@ func getInstruction (line string) (string, string) {
 	return line, ""
 }
 
-func numberToLittleEndian32 (number string) ([]byte) {
+func numberToLittleEndian (number string) ([]byte) {
 	intNumber, _ := strconv.Atoi(number)
 
-	// NOTE: After several debugging process, integer that is more than 127 needs zero padding
+	// NOTE: After several debugging process, integer that is more than 127 needs zero padding (32 bit).
 	if intNumber <= 127 {
 		byteIntNumber := byte(intNumber)
 		result := make([]byte, 1)
 		result[0] = byteIntNumber
 		return result
-	} else {
+	} else if intNumber <= 4294967295 {
 		byteIntNumber := uint32(intNumber)
 		result := make([]byte, 4)
 		binary.LittleEndian.PutUint32(result, byteIntNumber)
+		return result
+	} else {
+		byteIntNumber := uint64(intNumber)
+		result := make([]byte, 8)
+		binary.LittleEndian.PutUint64(result, byteIntNumber)
 		return result
 	}
 }
@@ -74,41 +79,27 @@ func numberToLittleEndian32 (number string) ([]byte) {
 func compileBytecodeToAssembly (instruction string, value string) ([]byte) {
 	var assembly_code []byte
 
-	if instruction == "TAMPILKAN_FROM_STACK" {
-		/*
-		 * NOTE: The x86_64 machine code is obtained from the assembly code below. In case of verification, do the steps below:
-		 * 
-		 *       1. Compile the assembly code below with `nasm -f bin [assembly file that contains the code below] -o [output file name]`
-		 *       2. Obtain the hex byte with `cat [output file name] | hd -b`
-		 * 
-		 * Assembly code
-		 *      |
-		 *     \|/
-		 *      v
-		 * ```
-		 * BITS 64
-		 * 
-		 * mov rdx, rsi ; rdx/3rd argument for write() syscall, string length, come from rsi/2nd argument of assemblyPrintFunction()
-		 * mov rsi, rdi ; rsi/2nd argument for write() syscall, string address, come from rdi/1st argument of assemblyPrintFunction()
-		 * 
-		 * ; prevent null-byte when inserting 1 into rdi & rax with INC instruction 
-		 * xor rdi, rdi ; empty rdi
-		 * inc rdi      ; rdi = 1, stdout
-		 * xor rax, rax ; empty rax
-		 * inc rax      ; rax = 1, write()
-		 * syscall
-		 * ret
-		 * ```
-		 *
-		 */
-		assembly_code = append(assembly_code, []byte{
-			0x48, 0x89, 0xf2, 0x48, 0x89, 0xfe, 0x48,
-			0x31, 0xff, 0x48, 0xff, 0xc7, 0x48, 0x31,
-			0xc0, 0x48, 0xff, 0xc0, 0x0f, 0x05, 0xc3,
-		}...)
-	} else if instruction == "PUSH" {
+	/*
+	 * NOTE: The x86_64 machine code is obtained from the assembly code below. In case of verification, do the steps below:
+	 * 
+	 *       1. Compile the assembly code below with `nasm -f bin [assembly file that contains the code below] -o [output file name]`
+	 *       2. Obtain the hex byte with `cat [output file name] | hd -b`
+	 * 
+	 * Assembly code
+	 *      |
+	 *     \|/
+	 *      v
+	 * ```
+	 * BITS 64
+	 *
+	 * ;assembly code here;
+	 *
+	 * ```
+	 * 
+	 */
+	if instruction == "PUSH" {
 		assembly_code  = append(assembly_code, []byte{0x6a}...) // machine code of 'push'
-		assembly_code  = append(assembly_code, numberToLittleEndian32(value)...)
+		assembly_code  = append(assembly_code, numberToLittleEndian(value)...)
 	} else if instruction == "TAMBAH" {
 		/*
 		 * BITS 64
@@ -157,11 +148,12 @@ func compileBytecodeToAssembly (instruction string, value string) ([]byte) {
 		// for return, value to be returned is stored in rax, so the code will be 'pop rax \n ret'
 		assembly_code  = append(assembly_code, []byte{0x58, 0xc3}...)
 	} else {
-		assembly_code  = append(assembly_code, []byte{0x90}...) // machine code of 'pop'
+		assembly_code  = append(assembly_code, []byte{0x90}...) // machine code of 'nop'
 	}
 
 	return assembly_code
 }
+
 
 type executableFunction func() (int)
 func executeAssembly (bytecodes []byte) (int) {
@@ -182,41 +174,98 @@ func executeAssembly (bytecodes []byte) (int) {
 
 	unsafeAssemblyFunction := (uintptr)(unsafe.Pointer(&assemblyFunctionMmap))
 	assemblyFunction       := *(*executableFunction)(unsafe.Pointer(&unsafeAssemblyFunction))
-	return assemblyFunction()
+	result                 := assemblyFunction()
+
+	// Unmap the mapped memories
+	syscall.Munmap(assemblyFunctionMmap)
+
+	return result
 }
 
-type printFunction func(data *[]byte, length int)
-func assemblyPrintFunction (bytecodes []byte, stringData string, stringLength int) {
+//type printFunction func(data *[]byte, length int)
+type printFunction func()
+func assemblyPrintFunction (stringData string) {
 
-	// This mmap is used for storing stringData into memory address that could be accessed by assemblyFunctionMmap
-	stringDataMmap, dataErr := syscall.Mmap(
-		-1, 0, len(stringData),
-		syscall.PROT_READ | syscall.PROT_WRITE,
-		syscall.MAP_PRIVATE | syscall.MAP_ANONYMOUS,
-	)
+	/*
+	 * NOTE: As golang mmap() is different from C mmap(), string is inserted inside the bytecode instead
+	 *       of accessing the string address from RSI register directly when calling the mmap function.
+	 *
+	 *
+	 * C example:
+	 *
+	 * typedef void(*printFunction)(char *msg, size_t msg_len)
+	 *
+	 * void assemblyPrintFunction(char *msg) {
+	 *   char opcode[]     = ".....";   <- contains instruction with accessing RSI and RDX
+	 *   size_t opcode_len = strlen(opcode);
+	 *   size_t msg_len    = strlen(msg);
+	 *
+	 *   void *code = mmap(NULL, opcode_len, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	 *
+	 *   if (code == MAP_FAILED) { return 1; }
+	 *
+	 *   memcpy(code, opcode, opcode_len);
+	 *   ((printFunction)code)(msg, msg_len);
+	 *
+	 * }
+	 *
+	 */
+
+	stringLengthLE32 := make([]byte, 4)
+	binary.LittleEndian.PutUint32(stringLengthLE32, uint32(len(stringData)))
+
+	/*
+	 * Assembly of machine code below:
+	 *
+	 * xor rax, rax
+	 * inc rax      ; rax = 1
+	 * xor rdi, rdi
+	 * inc rdi      ; rdi = 1
+	 *
+	 */
+	customBytecodes := []byte{0x48, 0x31, 0xc0, 0x48, 0xff, 0xc0, 0x48, 0x31, 0xff, 0x48, 0xff, 0xc7}
+	customBytecodes = append(customBytecodes, []byte{0x48, 0xc7, 0xc2}...)                         // mov rdx, .... <- 32 bit value
+	customBytecodes = append(customBytecodes, stringLengthLE32...)                                 // 32 bit value / length of stringData
+	customBytecodes = append(customBytecodes, []byte{0x48, 0x8d, 0x35, 0x03, 0x00, 0x00, 0x00}...) // lea rsi, [rip+0x03]
+	/*
+	 * Assembly of machine code below:
+	 *
+	 * syscall
+	 * ret
+	 *
+	 */
+	customBytecodes = append(customBytecodes, []byte{0x0f, 0x05, 0xc3}...)
+
+	// Insert the string into the machine code
+	for i := range stringData {
+		customBytecodes = append(customBytecodes, []byte{byte(stringData[i])}...)
+	}
+	// Append carriage return + new line + null terminator after the string
+	customBytecodes = append(customBytecodes, []byte{0x0d, 0x0a, 0x00}...) // \r\n\00
 
 	assemblyFunctionMmap, err := syscall.Mmap(
-		-1, 0, len(bytecodes),
+		-1, 0, len(customBytecodes),
 		syscall.PROT_READ | syscall.PROT_WRITE | syscall.PROT_EXEC,
 		syscall.MAP_PRIVATE | syscall.MAP_ANONYMOUS,
 	)
 
-	if (dataErr != nil) || (err != nil) {
-		fmt.Println("[-] ERROR: error in executeAssembly()")
+	if err != nil {
+		fmt.Println("[-] ERROR: Can't create memory mapping for constructed machine code in assemblyPrintFunction()")
 		os.Exit(1)
 	}
-
-	for i := range bytecodes {
-		assemblyFunctionMmap[i] = bytecodes[i]
-	}
-
-	for i := range stringData {
-		stringDataMmap[i] = stringData[i]
+	
+	for i := range customBytecodes {
+		assemblyFunctionMmap[i] = customBytecodes[i]
 	}
 
 	unsafeAssemblyFunction := (uintptr)(unsafe.Pointer(&assemblyFunctionMmap))
 	assemblyFunction       := *(*printFunction)(unsafe.Pointer(&unsafeAssemblyFunction))
-	assemblyFunction(&stringDataMmap, stringLength)
+	assemblyFunction()
+
+	syscall.Munmap(assemblyFunctionMmap)
+
+	// TODO: Debug the issue of new line doesn't appear on screen even though it has been added in customBytecodes
+	fmt.Println("")
 }
 
 /* Program entry point */
@@ -259,17 +308,19 @@ func main () {
 					currentBytecodes := append(currentBytecodes, returnBytecode...)
 					result           := strconv.Itoa(executeAssembly(currentBytecodes))
 					currentBytecodes = currentBytecodes[:0] // reset bytcode container after executing it
-					fmt.Println(result)
 
 					nextInstructionStatus, nextInstructionValue := virtualStack.Pop()
 					if nextInstructionStatus && (nextInstructionValue == "TAMPILKAN_FROM_STACK"){
-						currentBytecodes = compileBytecodeToAssembly(nextInstructionValue, "")
-						assemblyPrintFunction(currentBytecodes, result, len(result))
+						assemblyPrintFunction(result)
 						currentBytecodes = currentBytecodes[:0]
 					} else {
 						// Push the result into stack with "PUSH" bytecode
 						virtualStack.Push("PUSH " + result)
 					}
+				} else if status && (instruction == "TAMPILKAN"){
+					stringValue := value[1:len(value)-1] // take the string value inside '
+					assemblyPrintFunction(stringValue)
+					currentBytecodes = currentBytecodes[:0]
 				} else {
 					currentBytecodes = append(currentBytecodes, compileBytecodeToAssembly(instruction, value)...)
 				}
